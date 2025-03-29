740a7d1c1e2c8a630dc8fb64c1423a94c72da1d6
justnapinn <jinnaphatpansri@gmail.com>
2025-03-29 09:18:32 +0700

    Break into chunks

---
+++ chatbot.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+#Upload PDF files
+st.header("My First Chatbot")
+
+with st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader("Upload a PDF file and start asking question",type="pdf")
+
+#Extract the text
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text() #Concatenate text from all pages into a single string in the text variable.
+        #st.write(text)
+
+#Break it into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators = "\n",
+        chunk_size = 1000,
+        chunk_overlap = 150,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(text)
+    st.write(chunks)
